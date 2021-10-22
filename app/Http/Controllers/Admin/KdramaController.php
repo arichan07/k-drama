deaa37182a6f91f74a4986c7f96accdd2a29363e
Arisa <rin.a.27@icloud.com>
2021-10-22 07:45:17 +0000

    Controller.php

--- app/Http/Controllers/Admin/KdramaController.php
+++ app/Http/Controllers/Admin/KdramaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Kdrama; 
 
 class KdramaController extends Controller
 {
@@ -11,10 +12,28 @@ class KdramaController extends Controller
     {
         return view('admin.kdrama.create');
     }
+    
   
     public function create(Request $request)
     {
+        $this->validate($request, Kdrama::$rules);
+        ​
+        $kdrama = new Kdrama;
+        $form = $request->all();
+        ​
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $kdrama->image_path = null;
+        }
+        ​
+        unset($form['_token']);
+        unset($form['image']);
+        ​
+        $kdrama->fill($form);
+        $kdrama->save();
+        
         return redirect('admin.kdrama.create');
     }
-  
 }
