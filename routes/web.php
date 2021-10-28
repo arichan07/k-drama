c343a4f86f10a8ae824ff8a692a64256c99eb128
Arisa <rin.a.27@icloud.com>
2021-10-28 08:56:53 +0000

    web.php

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,12 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
 
     Route::get('/home', 'HomeController@index')->name('home');
-    Route::get('kdrama/create', 'Admin\KdramaController@add')->middleware('auth');
-    Route::post('kdrama/create', 'Admin\KdramaController@create')->middleware('auth');
     Route::get('kdrama', 'Admin\KdramaController@index')->middleware('auth'); 
-    
+    Route::get('kdrama/create', 'Admin\KdramaController@add')->middleware('auth');
+    Route::get('kdrama/edit', 'Admin\KdramaController@edit')->middleware('auth');
+    Route::post('kdrama/create', 'Admin\KdramaController@create')->middleware('auth');
+    Route::post('kdrama/edit', 'Admin\KdramaController@update')->middleware('auth');
+
 });
 
 Auth::routes();
