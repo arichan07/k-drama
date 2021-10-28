f8f1254628a9876e964768394e4a4861647df745
Arisa <rin.a.27@icloud.com>
2021-10-28 06:36:33 +0000

    web.php

--- app/Kdrama.php
+++ app/Kdrama.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Kdarama extends Model
+class Kdrama extends Model
 {
     protected $guarded = array('id');
 
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin'], function() {
+
+    Route::get('/home', 'HomeController@index')->name('home');
     Route::get('kdrama/create', 'Admin\KdramaController@add')->middleware('auth');
     Route::post('kdrama/create', 'Admin\KdramaController@create')->middleware('auth');
+
 });
+
+Auth::routes();
 
 Auth::routes();
 
