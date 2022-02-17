510d109cb76507521bbffd10431c5c7abc12555c
Arisa <rin.a.27@icloud.com>
2022-02-17 14:14:19 +0000

    admin.blade

--- resources/views/admin/kdrama/index.blade.php
+++ resources/views/admin/kdrama/index.blade.php
@@ -7,6 +7,20 @@
             <div class="col-md-3">
                 <a href="{{ action('Admin\KdramaController@add') }}" role="button" class="btn btn-primary">新規作成</a>
             </div>
+            <div class="col-md-8">
+                <form action="{{ action('Admin\KdramaController@index') }}" method="get">
+                    <div class="form-group row">
+                        <label class="col-md-3">ドラマタイトル</label>
+                        <div class="col-md-5">
+                            <input type="text" class="form-control" name="cond_title" value={{ $cond_title }}>
+                        </div>
+                        <div class="col-md-2">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="検索">
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
         <div class="row">
             <h2>ドラマ一覧</h2>
@@ -14,7 +28,7 @@
         <div class="row">
             <div class="admin-news col-md-12 mx-auto">
                 <div class="row">
-                    <table class="table table-dark">
+                    <table class="table table-light">
                         <thead>
                             <tr>
                                 <th width="10%">ID</th>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,47 +17,25 @@
         <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
     </head>
     <body>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light style="background-color: pink; navbar-expand-lg navbar-light bg-light rounded-bottom">
-            <a class="navbar-brand" href="#">Top</a>
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="Navber">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home<span class="sr-only">(現位置)</span></a>
-                        </li>
-                    </ul>    
-                    <form class="form-inline my-2 my-lg-0">
-                        <form action="{{ action('Admin\KdramaController@index') }}" method="get">
-                            <div class="form-group row">
-                                <nav class="navbar navbar-light bg-light">
-                                    <form class="form-inline">
-                                        <input type="text" class="form-control" name="cond_title" placeholder="検索..." aria-label="検索..." value={{ $cond_title }}>
-                                        <button type="submit" class="btn btn-outline-success my-2 my-sm-0">検索</button>
-                                    </form>
-                                </nav>
-                                    {{ csrf_field() }}    
                         @guest
-                            <ul><a class="nav-link ml-auto" href="{{ route('login') }}">{{ __('Login') }}</a></ul>
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                         @else
-                            <ul class="nav-item dropdown ml-auto">
+                            <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
+                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
-                            </ul>
-                        @endguest
+                            </li>
+                            @endguest
                         </ul>
                     </div>
                 </div>
@@ -65,6 +43,6 @@
             <main class="py-4">
                 @yield('content')
             </main>
-        </div>
+        </div>    
     </body>
 </html>
